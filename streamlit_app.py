4c1403a4e81912b569493158fe0bdb474f551abf
jain22vipul <138384753+jain22vipul@users.noreply.github.com>
2023-07-07 20:06:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,4 @@ streamlit.header("The Fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
-streamlit.write('The user entered ', add_my_fruit)
+streamlit.write('Thanks for Adding ', add_my_fruit)
